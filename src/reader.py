5e5d0b8c86736f697923c9eac7322e9e5e9b8664
Berat Çimen <beratcmn@gmail.com>
2022-03-19 20:39:25 +0300

    Reader artık satırların seviyelerini anlayabiliyor

--- src/reader.py
+++ src/reader.py
@@ -5,11 +5,26 @@ class Reader:
     def ReadInputCode():
         #input_file = open("gramer.kr", "r", encoding="utf-8")
         input_file = open(
-            r'C:\Users\\berat\Projects\Python\krypton-beta\gramer\gramer1.kr',
+            r'C:\Users\\berat\Projects\Python\krypton-beta\gramer\gramer2.kr',
             "r", encoding="utf-8")  # ? Debug
 
         input_code = input_file.read().rstrip()
 
+        lines = input_code.splitlines()
+        line_count = len(lines)
+        currentLevel = 0
+        levels = []
+        for i in range(0, line_count):
+            levels.append([i, currentLevel])
+
+            if "{" in lines[i]:
+                currentLevel = currentLevel + 1
+            if "}" in lines[i]:
+                currentLevel = currentLevel - 1
+
+        # for i in levels:
+        #     print(i)
+
         input_file.close()
 
-        return input_code
+        return input_code, levels
