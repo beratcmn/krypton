9db51e753d3f3aac0651c215a1fa3064f1ac91d1
Berat Çimen <beratcmn@gmail.com>
2022-03-20 01:54:24 +0300

    String hash

--- src/lexer.py
+++ src/lexer.py
@@ -8,11 +8,31 @@ class Lexer:
         "COMMENT",              # 3
     ]
 
+    stringHashes = {}
+
     def Tokenize(_inputCode: str, _levels: list):
 
         lines = [l.strip() for l in _inputCode.splitlines()]
 
         tokens = []
+
+        # ? String hashes
+        for line in lines:
+            start = 0
+            end = 0
+            xline = line
+            while end <= len(xline):
+                if xline[end:end+1] == '"':
+                    while '"' in xline:
+                        start = xline.index('"')
+                        xline = xline.replace('"', "", 1)
+                        end = xline.index('"')
+                        xline = xline.replace('"', "", 1)
+                        Lexer.stringHashes[hash(xline[start: end])] = xline[start: end]
+                        xline = xline.replace(xline[start: end], str(hash(xline[start: end])))
+                end = end + 1
+
+            lines[lines.index(line)] = xline
 
         for line in lines:
             start = 0
@@ -27,15 +47,21 @@ class Lexer:
                     newToken.append(line[2:len(line)].strip())
                     break
 
+                # ? Brackets
+                if line == "{":
+                    newToken.append("LEFT_BRACKET")
+                    newToken.append("{")
+                    break
+                if line == "}":
+                    newToken.append("RIGHT_BRACKET")
+                    newToken.append("}")
+                    break
+
                 # ? Class
                 if line[start:end] == "sınıf":
                     newToken.append(Lexer.tokenTypes[0])
-                    start = end
-
-                    while end <= len(line):
-                        if line[end-1:end] == "{":
-                            newToken.append(line[start:end-1].strip())
-                        end = end + 1
+                    newToken.append(line.partition("sınıf")[2].replace("{", "").strip())
+                    break
 
                 # ? Def
                 if line[start:end] == "fonksiyon":
@@ -50,16 +76,21 @@ class Lexer:
                             while line[end-1:end] != ")":
                                 end = end + 1
 
-                            newToken.append("PROPS="+line[start:end-1])
+                            newToken.append(line[start:end-1].split(","))  # ? props
 
                         end = end + 1
 
                 # ? Invoke Def
-                # if line[0:end]
+                if line[start:end] != "fonksiyon" and line[end-1:end] == "(":
+                    newToken.append(Lexer.tokenTypes[2])
+                    newToken.append(line[start:end-1])
+                    start = end
+                    while line[end-1:end] != ")":
+                        end = end + 1
 
-                else:
-                    end = end + 1
+                    newToken.append(line[start:end-1].split(","))  # ? props
+
+                end = end + 1
 
             tokens.append(newToken)
-
         return tokens
