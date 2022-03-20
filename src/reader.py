36a57c3940ee2a9bc89a3e5e6079e0e66bfe5146
Berat Çimen <beratcmn@gmail.com>
2022-03-20 17:16:25 +0300

    Test

--- src/reader.py
+++ src/reader.py
@@ -5,7 +5,7 @@ class Reader:
     def ReadInputCode():
         #input_file = open("gramer.kr", "r", encoding="utf-8")
         input_file = open(
-            r'C:\Users\\berat\Projects\Python\krypton-beta\gramer\gramer2.kr',
+            r'C:\Users\\berat\Projects\Python\krypton-beta\gramer\gramer1.kr',
             "r", encoding="utf-8")  # ? Debug
 
         input_code = input_file.read().rstrip()
