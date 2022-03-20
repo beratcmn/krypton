04000fac5a9dc8c1ea4c7d5dba4d679b11ff7485
Berat Çimen <beratcmn@gmail.com>
2022-03-20 16:44:24 +0300

    debug için ufak şeyler ekledim

--- src/main.py
+++ src/main.py
@@ -8,13 +8,22 @@ from writer import Writer
 from shell import Shell
 
 
-def main():
-    input_code = Reader.ReadInputCode()[0]
-    levels = Reader.ReadInputCode()[1]
-    tokens = Lexer.Tokenize(_inputCode=input_code, _levels=levels)
-
+def ShowTokens(tokens, levels):
     for i in range(0, len(tokens)):
         print(str(i+1) + ": " + str(tokens[i]) + " Level: " + str(levels[i][1]))
+
+
+def main():
+    input_code, levels = Reader.ReadInputCode()
+    tokens, string_hashes = Lexer.Tokenize(_inputCode=input_code, _levels=levels)
+    parsed_code = Parser.Parse(tokens, levels, string_hashes)
+
+    print("Source Code:")
+    print(input_code)
+    print("\nLex Result:")
+    ShowTokens(tokens, levels)
+    print("\nParse Result:")
+    print(parsed_code)
 
 
 if __name__ == "__main__":
