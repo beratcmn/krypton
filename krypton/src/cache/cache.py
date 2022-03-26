be56abe1bd5ada6d1d421d4bdbb1d2ee8904e940
Berat Çimen <beratcmn@gmail.com>
2022-03-26 19:30:56 +0300

    Dosya yapısını güncelledim

---
+++ krypton/src/cache/cache.py
@@ -0,0 +1,7 @@
+class Program:
+    # Programın buradan başlıyor
+    def Main():
+        print("Merhaba, Dünya!")
+
+if __name__ == "__main__":
+    Program.Main()
--- run.ps1
+++ run.ps1
@@ -1,3 +1,3 @@
 Clear-Host
 # python3.10 "./src/main.py" "./gramer/gramer2.kr"
-python3.10 "./src/main.py" "./language_tests/deneme1.kr"
+python3.10 "./krypton/src/main.py" "./language_tests/deneme1.kr"
