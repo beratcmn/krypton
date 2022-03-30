53d73c64439119b7bafd62986d66e24891c74adc
Berat Çimen <beratcmn@gmail.com>
2022-03-31 00:29:38 +0300

    Test dosyalarını gizledim

--- krypton/src/cache/cache.py
+++ krypton/src/cache/cache.py
@@ -1,20 +1,7 @@
-def test2():
-    pass
 def Main():
-    x = 5
-    y = 6
-    berat = None
-    def test1():
-        print("test1")
-    test1()
-    if (x>y):
-        print("sa")
-    elif (x<y):
-        print("as")
-    else:
-        print("xd")
-        if (1<2):
-            print("helal")
+    xd()
+def xd():
+    print("xd")
 
 if __name__ == "__main__":
     Main()
