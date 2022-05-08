8527ff0c1ee0033ff3b16b77a4c3e44c1bb63104
Berat Çimen <beratcmn@gmail.com>
2022-05-08 19:29:03 +0300

    Parser now parses print function

--- krypton/src/parser.py
+++ krypton/src/parser.py
@@ -27,7 +27,16 @@ def ParsePair(pair: Pair):
         line = pair.line.level * " " + pair.token.name + " = " + ParseToken(pair.token.value)
         return line
     elif isinstance(pair.token, INVOKE_FUNCTION):
-        line = pair.line.level * " " + pair.token.function_name + "("
+        function_name = pair.token.function_name
+        if pair.token.function_name == "yazdır":
+            function_name = "print"
+        elif pair.token.function_name == "karekök":
+            function_name = "print"  # ! CHANGE LATER
+        else:
+            function_name = pair.token.function_name
+
+        line = pair.line.level * " " + function_name + "("
+
         for p in pair.token.function_params:
             if pair.token.function_params.index(p) != len(pair.token.function_params) - 1:
                 line = line + ParseToken(p) + ", "
@@ -41,7 +50,16 @@ def ParsePair(pair: Pair):
 def ParseToken(token: Token):
     if isinstance(token, INVOKE_FUNCTION):
         token: INVOKE_FUNCTION = token
-        word = token.function_name + "("
+
+        function_name = token.function_name
+        if token.function_name == "yazdır":
+            function_name = "print"
+        elif token.function_name == "karekök":
+            function_name = "print"  # ! CHANGE LATER
+        else:
+            function_name = token.function_name
+
+        word = function_name + "("
         for p in token.function_params:
             if token.function_params.index(p) != len(token.function_params) - 1:
                 word = word + str(p) + ", "
